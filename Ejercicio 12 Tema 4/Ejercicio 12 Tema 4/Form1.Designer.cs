6136ab3ff7b1dcd3132403d4a6f18a53e53c9303
Salvador P <salvadorperezd@gmail.com>
2023-12-09 19:52:36 +0100

    Codigo inicial

---
+++ Ejercicio 12 Tema 4/Ejercicio 12 Tema 4/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Ejercicio_12_Tema_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fechaBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(287, 71);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(100, 26);
+            this.txtDia.TabIndex = 0;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(287, 125);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(100, 26);
+            this.txtMes.TabIndex = 1;
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(287, 178);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(100, 26);
+            this.txtAno.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Día:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mes:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(200, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Año:";
+            // 
+            // fechaBtn
+            // 
+            this.fechaBtn.BackColor = System.Drawing.Color.Coral;
+            this.fechaBtn.Location = new System.Drawing.Point(252, 243);
+            this.fechaBtn.Name = "fechaBtn";
+            this.fechaBtn.Size = new System.Drawing.Size(149, 61);
+            this.fechaBtn.TabIndex = 6;
+            this.fechaBtn.Text = "¿Es válida la fecha?";
+            this.fechaBtn.UseVisualStyleBackColor = false;
+            this.fechaBtn.Click += new System.EventHandler(this.fechaBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fechaBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtDia);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 12 Tema 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button fechaBtn;
+    }
+}
+
---
+++ Ejercicio 12 Tema 4/Ejercicio 12 Tema 4/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_12_Tema_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool bisiesto(int ano)
+        {
+            int resto4;
+            int resto100;
+            int resto400;
+            bool bisiesto = false;
+
+            resto4 = ano % 4;
+            resto100 = ano % 100;
+            resto400 = ano % 400;
+            //Si es divisible entre 4 y no entre 100 consideramos que es bisiesto.
+            if (resto4 == 0 && resto100 != 0)
+            {
+                bisiesto = true;
+            }
+            //Si es divisible entre 400 es bisiesto.
+            if (resto4 == 0 && resto400 == 0)
+            {
+                bisiesto = true;
+            }
+            return bisiesto;
+        }
+
+        bool fechaValida(int dia, int mes, int ano)
+        {
+            bool fecha = false;
+            bool diaCorrecto = false;
+            bool mesCorrecto = mes > 0 && mes <= 12;
+
+            if (mes == 4 || mes == 6 || mes == 9 || mes == 11)
+            {
+                diaCorrecto = dia > 0 && dia <= 30;
+            }
+            if (mes == 2)
+            {
+                diaCorrecto = dia > 0 && dia <= 28;
+                if (bisiesto(ano) == true)
+                {
+                    diaCorrecto = dia > 0 && dia <= 29;
+                }
+            }
+            if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12)
+            {
+                diaCorrecto = dia > 0 && dia <= 31;
+            }
+            if (ano >= 0 && mesCorrecto && diaCorrecto)
+            {
+                fecha = true;
+            }
+            return fecha;
+        }
+        private void fechaBtn_Click(object sender, EventArgs e)
+        {
+            int dia;
+            int mes;
+            int ano;
+            string texto = "La fecha introducida no es válida.";
+
+            dia = int.Parse(txtDia.Text);
+            mes = int.Parse(txtMes.Text);
+            ano = int.Parse(txtAno.Text);
+
+            if (fechaValida(dia, mes, ano) == true)
+            {
+                texto = "La fecha introducida es válida.";
+            }
+            MessageBox.Show(texto);
+        }
+    }
+}
